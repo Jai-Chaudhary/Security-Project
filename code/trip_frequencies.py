a3fcde2364e0a7526165dacbc8f63a53b07986e9
Anton Kueltz <kueltz.anton@gmail.com>
2014-12-08 16:11:27 -0500

    Overall graphs for targets

--- code/trip_frequencies.py
+++ code/trip_frequencies.py
@@ -16,6 +16,7 @@ days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def plot_freq(data, bank):
 	for name, target in zip(names, data):
+		print "Plotting {} {}".format(bank, name)
 		xs, ys = range(len(target)), target
 		plt.clf()
 		plt.plot(xs, ys)
@@ -24,6 +25,27 @@ def plot_freq(data, bank):
 		plt.title(bank + " " + name)
 		pylab.savefig("../figures/{}/{}.png".format(bank, name))
 
+def plot_targets(targets):
+	for name, target in zip(names, targets):
+		xs, ys = range(len(target)), target
+		plt.clf()
+		plt.plot(xs, ys)
+		plt.xlabel("Day")
+		plt.ylabel("Trips")
+		plt.title(name)
+		pylab.savefig("../figures/Targets/{}.png".format(name))
+
+def add_arrays(a1, a2):
+	if not a1: return a2
+	elif not a2: return a1
+	else:
+		pairs = zip(a1, a2)
+		a3 = [0]*len(pairs)
+		i = 0
+		for v1, v2 in pairs:
+			a3[i] = v1+v2
+			i += 1
+		return a3	
 
 def date_as_daynum(date):
 	month, day = int(date[5:7]), int(date[8:10])
@@ -62,7 +84,12 @@ def process_csv(csv_num):
              "UBS1", "UBS2", "BOA", "LAZ", "BNP", "SOCG", "WAC", "MAQ"]
 	bank = banks[csv_num-1]
 	plot_freq(bins, bank)
+	return bins
 
 if __name__ == "__main__":
+	ts = [[] for _ in range(len(targets))]
 	for csv_num in range(1, 17):
-		process_csv(csv_num)
+		target_data = process_csv(csv_num)
+		for i, t in enumerate(target_data):
+			ts[i] = add_arrays(ts[i], t)
+	plot_targets(ts)
